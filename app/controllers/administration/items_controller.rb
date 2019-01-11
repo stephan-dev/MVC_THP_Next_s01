be51f2c923b9ad0b53a9223ab8476087965c6d6e
stephan-dev <stephan-dev@outlook.fr>
2019-01-11 15:25:11 +0100

    update discount method

--- app/controllers/administration/items_controller.rb
+++ app/controllers/administration/items_controller.rb
@@ -7,7 +7,21 @@ module Administration
     end
 
     def update
+      @item = Item.find(params[:id])
+      @item.update(item_params)
+      if @item[:discount_percentage].positive?
+        @item.update(has_discount: true)
+      else
+        @item.update(has_discount: false)
+      end
+
       redirect_to administration_items_path
+    end
+
+    private
+
+    def item_params
+      params.require(:item).permit(:discount_percentage)
     end
   end
 end
